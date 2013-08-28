6ae26eee1070c1a055ca2283cebad5821619fc8f
Felipe Reyes <freyes@tty.cl>
2013-08-28 17:51:03 -0400

    Added new list classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,14 @@ setup(
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Monitoring",
     ],
     entry_points="""
     [console_scripts]
